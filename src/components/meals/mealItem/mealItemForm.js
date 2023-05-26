6434e0a5d70df72dc27795188141245d7f9e6cce
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-05-27 04:20:53 +0530

    meal item form component added

---
+++ src/components/meals/mealItem/mealItemForm.js
@@ -0,0 +1,25 @@
+import Input from "../../UI/input";
+import classes from "./mealItemForm.module.css";
+
+const MealItemForm=() =>{
+    const onClickBtnHandler=(e) =>{
+
+    }
+    return(
+        <form className={classes.form}>
+             <div>
+                <Input label="QTY." input={{
+                    id: "qty",
+                    type: "number",
+                    min: '1',
+                    max: '5',
+                    step: '1',
+                    defaultValue: '1'
+                }}/>
+            </div>
+            <button onClick={onClickBtnHandler}>+ ADD</button>
+        </form>   
+    )
+}
+
+export default MealItemForm;
