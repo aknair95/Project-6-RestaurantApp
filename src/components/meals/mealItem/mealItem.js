77e908123a2e0c4d2ddaaa8a561def01978efe1a
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-05-27 04:21:46 +0530

    input re-usable component added to UI folder

--- src/components/meals/mealItem/mealItem.js
+++ src/components/meals/mealItem/mealItem.js
@@ -1,4 +1,5 @@
 import classes from "./mealItem.module.css";
+import MealItemForm from "./mealItemForm";
 
 const MealItem=(props) =>{
     return(
@@ -8,7 +9,7 @@ const MealItem=(props) =>{
                 <div className={classes.description}>{props.description}</div>
                 <div className={classes.price}>{props.price}</div>
             </div>
-            <div></div>
+            <div><MealItemForm/></div>
         </li>
     )
 }
